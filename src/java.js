46a0a0fbe7045835160b86f39722b6c638383c56
zandilemaleka <malekabaatile@gmail.com>
2025-04-13 19:33:49 +0200

    Added the AI API integration

--- src/java.js
+++ src/java.js
@@ -1,13 +1,29 @@
-function generateRecipe(event){
-event.preventDefault();
- // Clear previous recipe
+function displayRecipe(response) {
+    console.log("reciped generated");
 new Typewriter("#recipe", {
-  strings: "bread",
+  strings: response.data.answer,
   autoStart: true,
   cursor: null,
   delay: 1,
 });
+}    
+
+
+
+function generateRecipe(event){
+event.preventDefault();
+
+let InstructionInput = document.querySelector("#ingredients");
+let apiKey = "05b1af1a7feb6t6o414b06b73f1569ed";
+let prompt = `Generate a recipe for a dish using ${InstructionInput.value}`;
+let context = "You are a great cook, and love to come up with new recipes, that are easy to make and delicious. Put the name of the dish in the first line and inside a <strong/> element, and then write the recipe. Use a list format for the ingredients with <br/> element, and then write the instructions. Make sure to include cooking time and serving size. Make it fun and engaging.";
+let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+console.log(`Pormpt ${prompt}`);
+console.log(`Context ${context}`);
+axios.get(apiUrl).then(displayRecipe);
 }
+ // Clear previous recipe
+
 
 let recipeFormElement = document.querySelector("#recipe-form");
 recipeFormElement.addEventListener("submit", generateRecipe); 
